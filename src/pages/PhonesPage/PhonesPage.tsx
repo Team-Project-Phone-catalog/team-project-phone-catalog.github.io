9dd532874409376601dc66ab5c54f617912b1891
Hanna Torianyk <an.mol4anova@gmail.com>
2026-02-13 17:07:59 +0200

    feat(DEV-1234): added structure for product pages

---
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -0,0 +1,13 @@
+export const PhonesPage = () => {
+  return (
+    <div className="phones-page">
+      <h1 className="title">Mobile phones</h1>
+
+      <section className="phones-page__controls"></section>
+
+      <section className="phones-page__list"></section>
+
+      <section className="phones-page__pagination"></section>
+    </div>
+  );
+};
