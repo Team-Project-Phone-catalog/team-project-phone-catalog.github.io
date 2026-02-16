275c34dcda95c722f246c9c3365bb50e825b60b8
MaksOther <m7885091@gmail.com>
2026-02-16 13:05:34 +0200

    fixed npm run deploy

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,5 +1,5 @@
 import './ProductDetailsPage.scss';
-import ProductCard from '../../components/ui/Product/ProductCard/ProductCard.tsx';
+import { ProductCard } from '../../components/ui/Product/ProductCard/ProductCard.tsx';
 import { BackButton } from '../../components/ui/Buttons/Back/BackButton.tsx';
 
 export const ProductDetailsPage = () => {
