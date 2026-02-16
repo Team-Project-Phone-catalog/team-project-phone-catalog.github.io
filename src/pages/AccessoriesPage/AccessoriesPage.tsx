b70ab47c74222917e4aea72c3d36d2f16347fbb2
Hanna Torianyk <an.mol4anova@gmail.com>
2026-02-16 09:23:51 +0200

    feat(DEV-1234): products pages

--- src/pages/AccessoriesPage/AccessoriesPage.tsx
+++ src/pages/AccessoriesPage/AccessoriesPage.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
+import { getAccessories } from '../../api/products';
+import { Product } from '../../types/Product';
+import { ProductCard } from '../ProductCard/ProductCard';
+import './AccessoriesPage.module.scss';
+
 export const AccessoriesPage = () => {
+  const [accessories, setAccessories] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const loadTablets = async () => {
+      const data = await getAccessories();
+      setAccessories(data);
+    };
+
+    loadTablets();
+  }, []);
+
   return (
     <div className="accessories-page">
       <h1 className="title">Accessories</h1>
 
       <section className="accessories-page__controls"></section>
 
-      <section className="accessories-page__list"></section>
+      <section className="accessories-page__list">
+        {accessories.map((accessory) => (
+          <ProductCard key={accessory.id} />
+        ))}
+      </section>
 
       <section className="accessories-page__pagination"></section>
     </div>
--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
+import { getPhones } from '../../api/products';
+import { Product } from '../../types/Product';
+import { ProductCard } from '../ProductCard/ProductCard';
+import './PhonesPage.module.scss';
+
 export const PhonesPage = () => {
+  const [phones, setPhones] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const loadPhones = async () => {
+      const data = await getPhones();
+      setPhones(data);
+    };
+
+    loadPhones();
+  }, []);
+
   return (
     <div className="phones-page">
       <h1 className="title">Mobile phones</h1>
 
       <section className="phones-page__controls"></section>
 
-      <section className="phones-page__list"></section>
+      <section className="phones-page__list">
+        {phones.map((phone) => (
+          <ProductCard key={phone.id} />
+        ))}
+      </section>
 
       <section className="phones-page__pagination"></section>
     </div>
