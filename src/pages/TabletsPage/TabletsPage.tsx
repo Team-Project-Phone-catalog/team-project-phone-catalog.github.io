0c823442244c9cb8ebb04f3430ddc7086920346b
MaksOther <m7885091@gmail.com>
2026-02-17 12:18:36 +0200

    fixed styles

--- src/pages/TabletsPage/TabletsPage.tsx
+++ src/pages/TabletsPage/TabletsPage.tsx
@@ -18,43 +18,45 @@ export const TabletsPage = () => {
 
   return (
     <div className={s['tablets-page']}>
-      <h1 className={s.title}>Tablets</h1>
-      <p className={s.modelsCount}>{tablets.length} models</p>
+      <div className={s['tablets-page__container']}>
+        <h1 className={s.title}>Tablets</h1>
+        <p className={s.modelsCount}>{tablets.length} models</p>
 
-      <section className={s['tablets-page__controls']}>
-        <div className={s.controls}>
-          <div className={s.control}>
-            <label className={s.label}>Sort by</label>
+        <section className={s['tablets-page__controls']}>
+          <div className={s.controls}>
+            <div className={s.control}>
+              <label className={s.label}>Sort by</label>
 
-            <select className={s.select}>
-              <option>Newest</option>
-              <option>Alphabetically</option>
-              <option>Cheapest</option>
-            </select>
+              <select className={s.select}>
+                <option>Newest</option>
+                <option>Alphabetically</option>
+                <option>Cheapest</option>
+              </select>
+            </div>
+
+            <div className={s.control}>
+              <label className={s.label}>Items on page</label>
+
+              <select className={s.select}>
+                <option>16</option>
+                <option>32</option>
+                <option>64</option>
+              </select>
+            </div>
           </div>
+        </section>
 
-          <div className={s.control}>
-            <label className={s.label}>Items on page</label>
+        <section className={s['tablets-page__list']}>
+          {tablets.map((tablet) => (
+            <ProductCard
+              key={tablet.id}
+              product={tablet}
+            />
+          ))}
+        </section>
 
-            <select className={s.select}>
-              <option>16</option>
-              <option>32</option>
-              <option>64</option>
-            </select>
-          </div>
-        </div>
-      </section>
-
-      <section className={s['tablets-page__list']}>
-        {tablets.map((tablet) => (
-          <ProductCard
-            key={tablet.id}
-            product={tablet}
-          />
-        ))}
-      </section>
-
-      <section className={s['tablets-page__pagination']}></section>
+        <section className={s['tablets-page__pagination']}></section>
+      </div>
     </div>
   );
 };
